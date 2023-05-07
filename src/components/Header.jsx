3dc5addb29aef799bea38999d2d0417ff0e73035
YoheiYamashita <120314392+YoheiYamashita@users.noreply.github.com>
2023-05-07 16:58:59 +0900

    スクロール遷移

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,30 +10,46 @@ import { Link } from 'react-scroll';
 const macondoSwashCaps =css`
 @import url('https://fonts.googleapis.com/css?family=Macondo+Swash+Caps');`
 
-// const menu=css`
-// font-family:'Macondo+Swash+Caps';
-
-// font-size:20px;
-// list-style:none;
-
-
-// `;
 
 const headerList=css`
-display:flex;
-justify-content: space-around;
+margin:0 auto;
+padding:0;
+display:flex  ;
+flex-direction:row;
+justify-content:space-around;
+width:500px;
 
+@media  (max-width:500px){
+  //500px以下になったら
+  width:100%;
+}
+width:500px;
+
+
+;
 `
 const header = css`
 background-color: rgb(220, 191, 154);
 position:sticky;
 top:0;
 line-height:50px;
+width:100%;
+
 `
 const logo = css`
 list-style:none;
-font-size:30px
-font
+font-size:20px ;
+font-weight: 650;
+/* display:inline-block; */
+/* border-bottom: 1px solid; */
+text-decoration: underline;
+text-decoration-color: #362b01;
+text-underline-offset:0.5rem;
+padding:0px 5px 5px 20px;
+
+
+
+
 
 `
 
@@ -43,10 +59,10 @@ const Header = () => {
           {/* <style>{macondoSwashCaps}</style> */}
         <ul css={headerList}className='headerList'>    
             <li css={logo}className='logo'>YOHEI</li>
-            <li className='menu'><Link to="home" smooth={true} duration={500}>Home</Link></li>
-            <li  className='menu'>About</li>
-            <li  className='menu'>Works</li>
-            <li  className='menu'>Contact</li>
+            <li className='menu'><Link to="home" smooth={true} duration={700} offset={-70}>Home</Link></li>
+            <li  className='menu'><Link to="about" smooth={true} duration={700} >About</Link></li>
+            <li  className='menu'><Link to="works" smooth={true} duration={700} >Works</Link></li>
+            <li  className='menu'><Link to="contact" smooth={true} duration={700} >Contact</Link></li>
         </ul>
     </div>
   )
